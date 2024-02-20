be9027a136b0edb67b92071b5875327c2d224a52
pogoprojekt <147980675+pogoprojekt@users.noreply.github.com>
2024-02-20 14:30:23 +0100

    Add files via upload

---
+++ src/components/zaliczenie/AddProducts/ShopingList.jsx
@@ -0,0 +1,46 @@
+import commonColumnsStyles from "../../../common/styles/Columns.module.scss";
+import { useState } from "react";
+
+const ShoppingList = (props) => {
+  const { shoppingList, deleteProd } = props;
+  const [prodStyle, setProdStyle] = useState([]);
+
+  const handleRightMouseButtonClick = (event, idx) => {
+    event.preventDefault();
+    prodStyle[idx] !== "line-through"
+      ? (prodStyle[idx] = "line-through")
+      : (prodStyle[idx] = "none");
+    setProdStyle([...prodStyle]);
+  };
+
+
+  const handleDeleteProd = (idx) => {
+prodStyle.splice(idx, 1);
+setProdStyle([...prodStyle]);
+    deleteProd(idx);
+
+  };
+  return (
+    <div className={commonColumnsStyles.App}>
+      <header className={commonColumnsStyles.AppHeader}>
+        <p>Shoping List</p>
+        {shoppingList.map((prod, index) => (
+          <li
+
+            style={{
+              textDecoration: prodStyle[index] ? prodStyle[index] : "none", 
+          }}
+            key={prod.nazwa}
+            onContextMenu={(e) => handleRightMouseButtonClick(e, index)}
+            onClick={() => handleDeleteProd(index)}
+          >
+            {prod.nazwa}
+          </li>
+        ))}
+      </header>
+     
+    </div>
+  );
+}
+
+export default ShoppingList;
